d3ab85ac479e11ccd10c89e43d6bb6b41d10dce7
Francisco-0510 <francisco109623@gmail.com>
2024-11-26 10:23:45 -0600

    first build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Onest Variable", ...fontFamily.sans],
+      serif: ["Onest Variable", "Inter", "Satoshi", "Arial", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
