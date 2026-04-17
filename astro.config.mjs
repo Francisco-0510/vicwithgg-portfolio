f90a9bc8bdc02264469787757794fe8a9df9b508
Francisco Victorico Aguirre Jimenez <francisco109623@gmail.com>
2026-04-16 21:48:59 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
       },
     }),
     sitemap({
+      // ✅ Correcto
       filter: (page) =>
         !page.includes("/privacidad") &&
         !page.includes("/terminos") &&
